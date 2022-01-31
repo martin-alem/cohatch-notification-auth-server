5524c80fafc5214baac210135370a286f9a37afe
Martin Alemajoh <alemajohmartin@gmail.com>
2022-01-31 16:40:02 -0500

    fix cookie error

--- controllers/logoutController.js
+++ controllers/logoutController.js
@@ -2,7 +2,7 @@ import ErrorHandler from "../utils/ErrorHandler.js";
 
 function logoutController(req, res, next) {
   try {
-    res.clearCookie("_access_token", { domain: process.env.COOKIE_DOMAIN });
+    res.clearCookie("_access_token");
     res.status(204).json({ status: "success", statusCode: 204 });
   } catch (error) {
     next(new ErrorHandler(500, "Internal Server Error"));
